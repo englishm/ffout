a6ab28598775fd681bc5ac89f4fe19551074bb04
Mike English <mike.english@gmail.com>
2023-07-01 17:21:24 -0400

    WIP - convert to cargo-c

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,33 @@
-extern crate ffmpeg_sys_next;
+use std::error;
 
-use ffmpeg_sys_next::{AVFormatContext, AVPacket};
-use std::os::raw::c_int;
+type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
 
-#[no_mangle]
-pub extern "C" fn write_header(_ctx: *mut AVFormatContext) -> c_int {
-    // Implementation of write_header
-    0 // Return value, replace with actual implementation
+pub fn rust_fn() -> Result<()> {
+    Ok(())
 }
 
-#[no_mangle]
-pub extern "C" fn write_packet(_ctx: *mut AVFormatContext, _pkt: *mut AVPacket) -> c_int {
-    // Implementation of write_packet
-    0 // Return value, replace with actual implementation
+#[cfg(cargo_c)]
+pub mod capi {
+    use ffmpeg_sys_next::{AVFormatContext, AVPacket};
+    use std::os::raw::c_int;
+    #[no_mangle]
+    pub extern "C" fn write_header(_ctx: *mut AVFormatContext) -> c_int {
+        // Implementation of write_header
+        0 // Return value, replace with actual implementation
+    }
+
+    #[no_mangle]
+    pub extern "C" fn write_packet(_ctx: *mut AVFormatContext, _pkt: *mut AVPacket) -> c_int {
+        // Implementation of write_packet
+        0 // Return value, replace with actual implementation
+    }
+
+    #[no_mangle]
+    pub extern "C" fn write_trailer(_ctx: *mut AVFormatContext) -> c_int {
+        // Implementation of write_trailer
+        0 // Return value, replace with actual implementation
+    }
 }
 
-#[no_mangle]
-pub extern "C" fn write_trailer(_ctx: *mut AVFormatContext) -> c_int {
-    // Implementation of write_trailer
-    0 // Return value, replace with actual implementation
-}
+#[cfg(cargo_c)]
+pub use capi::*;
