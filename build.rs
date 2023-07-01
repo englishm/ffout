e696c4f7d6c7d73b9e1967ceac4f76af7bbc90e8
Mike English <mike.english@gmail.com>
2023-07-01 18:01:17 -0400

    Super hacky static pkg-config generation

--- build.rs
+++ build.rs
@@ -1,8 +1,10 @@
 extern crate cbindgen;
 
-use std::env;
+use std::{env, error, fs::File, io::Write};
 
-fn main() {
+type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
+
+fn main() -> Result<()> {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
     // cbindgen::Builder::new()
@@ -13,4 +15,21 @@ fn main() {
     cbindgen::generate(crate_dir)
         .expect("Unable to generate bindings")
         .write_to_file("target/release/libffout.h");
+
+    let pc_file_contents = r#"
+prefix=/usr/local
+exec_prefix=${prefix}
+libdir=${exec_prefix}/lib
+includedir=${prefix}/include
+
+Name: ffout
+Description: Demo ffmpeg output protocol library
+Version: 0.1.0
+Libs: -L${libdir} -lffout
+Cflags: -I${includedir}/ffout
+"#;
+
+    let mut pc_file = File::create("target/release/ffout.pc")?;
+    pc_file.write_all(pc_file_contents.as_bytes())?;
+    Ok(())
 }
