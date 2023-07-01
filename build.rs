553c7ea7e6ec85ca583fc07ba1f0e9c2b511f010
Mike English <mike.english@gmail.com>
2023-07-01 17:37:04 -0400

    Generate C header instead of C++

--- build.rs
+++ build.rs
@@ -5,9 +5,12 @@ use std::env;
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
-    cbindgen::Builder::new()
-        .with_crate(crate_dir)
-        .generate()
+    // cbindgen::Builder::new()
+    //     .with_crate(crate_dir)
+    //     .generate()
+    //     .expect("Unable to generate bindings")
+    //     .write_to_file("target/release/libffout.h");
+    cbindgen::generate(crate_dir)
         .expect("Unable to generate bindings")
         .write_to_file("target/release/libffout.h");
 }
