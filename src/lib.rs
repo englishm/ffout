d8262147043020564c6070af6c826ff1f2e8e7e3
Mike English <mike.english@gmail.com>
2023-08-22 17:10:57 -0400

    WIP: Start on URLProtocol implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,29 @@
 extern crate ffmpeg_sys_next;
 
-use ffmpeg_sys_next::{AVFormatContext, AVPacket};
-use std::os::raw::c_int;
+use ffmpeg_sys_next::{AVFormatContext, AVPacket, URLContext};
+use std::os::raw::{c_char, c_int, c_uchar};
+
+#[no_mangle]
+pub extern "C" fn ffout_open(
+    _url_ctx: *mut URLContext,
+    _url: *const c_char,
+    _flags: c_int,
+) -> c_int {
+    println!("hi from ffout_open");
+    0 // Return value, replace with actual implementation
+}
+
+#[no_mangle]
+pub extern "C" fn ffout_write(url_ctx: *mut URLContext, buf: *const c_uchar, size: c_int) -> c_int {
+    println!("hi from ffout_write");
+    println!("received {} bytes", &size);
+    println!("buf (ptr) {:?}", &buf);
+    let _slice_of_bytes = unsafe { std::slice::from_raw_parts(buf, size.try_into().unwrap()) };
+    dbg!(url_ctx);
+    //dbg!(slice_of_bytes);
+
+    0 // Return value, replace with actual implementation
+}
 
 #[no_mangle]
 pub extern "C" fn write_header(_ctx: *mut AVFormatContext) -> c_int {
